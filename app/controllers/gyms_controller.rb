f8a8f55bbedad1c36882da98b0c204a4ffbde0eb
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-10-08 15:45:32 +0300

    Adds the gymsshow and destroy methods

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,2 +1,13 @@
 class GymsController < ApplicationController
+
+    def show 
+        gym = Gym.find(params[:id])
+        render json: gym, status: :ok
+    end
+
+    def destroy
+        gym = Gym.find(params[:id])
+        gym.destroy
+        render json: {message: "Gym deleted"}, status: :ok
+    end
 end
